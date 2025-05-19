3b2d6d34e643e940943be32650171f81715e61aa
Mourad AUMAR <aumarmourad@gmail.com>
2025-05-19 10:46:46 +0200

    test failed for Herbal Tea

--- updateDrug.test.js
+++ updateDrug.test.js
@@ -1,4 +1,3 @@
-
 import { Drug } from "./pharmacy";
 import { updateDrug } from "./updateDrug";
 
@@ -30,5 +29,36 @@ describe("updateDrug – default case", () => {
     // benefit must down to -2
     expect(output.benefit).toBe(8);
   });
+});
 
+describe("updateDrug – Herbal Tea", () => {
+  it("increase benefit to 1 before expiration", () => {
+    const input = new Drug("Herbal Tea", 5, 10);
+    const output = updateDrug(input);
+
+    expect(input).toEqual(new Drug("Herbal Tea", 5, 10));
+
+    expect(output.expiresIn).toBe(4);
+    // benefit increase to +1
+    expect(output.benefit).toBe(11);
+  });
+
+  it("increase benefit to +2 after expiration", () => {
+    const input = new Drug("Herbal Tea", 0, 10);
+    const output = updateDrug(input);
+
+    expect(input).toEqual(new Drug("Herbal Tea", 0, 10));
+    expect(output.expiresIn).toBe(-1);
+
+    // benefit increase to +2
+    expect(output.benefit).toBe(12);
+  });
+
+  it("we never exceed 50", () => {
+    const input = new Drug("Herbal Tea", 5, 50);
+    const output = updateDrug(input);
+
+    // même après update, on clamp à 50
+    expect(output.benefit).toBe(50);
+  });
 });
