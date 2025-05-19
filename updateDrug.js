7181c5461b1e9004dc6d1063ec338569f90713f3
Mourad AUMAR <aumarmourad@gmail.com>
2025-05-19 10:21:39 +0200

    test update -1 before expiration for updateDrug

--- updateDrug.js
+++ updateDrug.js
@@ -1,3 +1,20 @@
+import { Drug } from "./pharmacy";
+
 export function updateDrug(drug) {
-  throw new Error("Not implemented");
+  // create new instance
+  const updated = new Drug(drug.name, drug.expiresIn, drug.benefit);
+
+  // before expiration decrement benefit -1
+  if (updated.benefit > 0) {
+    updated.benefit -= 1;
+  }
+
+  // decrement expiresIn
+  updated.expiresIn -= 1;
+
+  // after expiration -1 again
+  if (updated.expiresIn < 0 && updated.benefit > 0) {
+    updated.benefit -= 1;
+  }
+  return updated;
 }
--- updateDrug.test.js
+++ updateDrug.test.js
@@ -1,15 +1,16 @@
 //tester chaque cas 
 
 import { Drug } from "./pharmacy";
+import { updateDrug } from "./updateDrug";
 
 
 describe("updateDrug – default case", () => {
   it("decremente -1 before expiration date", () => {
-    const input = new Drug("Any",5,10);
+    const input = new Drug("Default",5,10);
     const output = updateDrug(input);
 
     // do not mutate
-    expect(input).toEqual(new Drug("Any", 5, 10));
+    expect(input).toEqual(new Drug("Default", 5, 10));
 
     // one day
     expect(output.expiresIn).toBe(4);
