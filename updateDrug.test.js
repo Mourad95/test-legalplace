b73c5261192ea3e1a9f13bd921cb17900482f68c
Mourad AUMAR <aumarmourad@gmail.com>
2025-05-19 10:26:07 +0200

    test update -2 after expiration for updateDrug

--- updateDrug.test.js
+++ updateDrug.test.js
@@ -1,12 +1,10 @@
-//tester chaque cas 
 
 import { Drug } from "./pharmacy";
 import { updateDrug } from "./updateDrug";
 
-
 describe("updateDrug – default case", () => {
   it("decremente -1 before expiration date", () => {
-    const input = new Drug("Default",5,10);
+    const input = new Drug("Default", 5, 10);
     const output = updateDrug(input);
 
     // do not mutate
@@ -18,4 +16,19 @@ describe("updateDrug – default case", () => {
     // benefit must down to -1
     expect(output.benefit).toBe(9);
   });
+
+  it("decremente -2 before expiration date", () => {
+    const input = new Drug("Default", 0, 10);
+    const output = updateDrug(input);
+
+    // do not mutate
+    expect(input).toEqual(new Drug("Default", 0, 10));
+
+    // one day
+    expect(output.expiresIn).toBe(-1);
+
+    // benefit must down to -2
+    expect(output.benefit).toBe(8);
+  });
+
 });
