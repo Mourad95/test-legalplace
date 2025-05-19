1d946e112b5f94f9591f4410467398ee0ff23016
Mourad AUMAR <aumarmourad@gmail.com>
2025-05-19 09:50:40 +0200

    init

---
+++ updateDrug.test.js
@@ -0,0 +1,9 @@
+//tester chaque cas 
+import { Drug } from "../src/pharmacy";
+import { updateDrug } from "../src/updateDrug";
+
+describe("updateDrug – normal case", () => {
+  it("décrémente le benefit de 1 avant la date d'expiration", () => {
+  
+  });
+});
